cc6e7c82de272907210e856d06573851928f7654
rabindra-basnet <120715649+rabindra-basnet@users.noreply.github.com>
2025-06-24 22:32:38 +0545

    fix:figured out and changed
    bench new-site --no-mariadb-socket to --mariadb-user-host-login-scope='%'

--- scripts/init.sh
+++ scripts/init.sh
@@ -39,7 +39,7 @@ sed -i '/redis/d' ./Procfile
 bench new-site dev.localhost \
 --mariadb-root-password 123 \
 --admin-password admin \
---no-mariadb-socket
+--mariadb-user-host-login-scope='%'
 
 bench --site dev.localhost set-config developer_mode 1
 bench --site dev.localhost clear-cache
