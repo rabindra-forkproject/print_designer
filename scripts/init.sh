b261680002c93e4d9e602139a09e761c84c0f41a
rabindra-basnet <120715649+rabindra-basnet@users.noreply.github.com>
2025-06-24 22:52:46 +0545

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -37,10 +37,11 @@ sed -i '/redis/d' ./Procfile
 
 
 bench new-site dev.localhost \
- --mariadb-root-username root
---mariadb-root-password 123 \
---admin-password admin \
---mariadb-user-host-login-scope='%'
+ --mariadb-root-username=root
+--mariadb-root-password=123 \
+--admin-password admin 
+# \
+# --mariadb-user-host-login-scope='%'
 
 bench --site dev.localhost set-config developer_mode 1
 bench --site dev.localhost clear-cache
